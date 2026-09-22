685c96d85fd8062a4a9d539f91919d624df8919b
Aliye <yaymuzikyapim2@gmail.com>
2026-09-22 16:20:04 +0300

    disable Vercel image optimization to avoid free tier limit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   serverExternalPackages: ["pg", "@prisma/adapter-pg", "@prisma/client"],
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
